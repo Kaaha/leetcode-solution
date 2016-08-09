924e1537ecc650f768875a82f7fb4d41fb8b1cac
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-09 13:19:45 +0800

    Update #011_ContainerWithMostWater.java

--- src/#011_ContainerWithMostWater.java
+++ src/#011_ContainerWithMostWater.java
@@ -1,11 +1,11 @@
 public class Solution {
-    public int maxArea(int[] nums){
+    public int maxArea(int[] height){
         int area=0;
-        int n=nums.length;
+        int n=height.length;
         int i=0,j=n-1;
         while(i<j){
-            int left=nums[i];
-            int right=nums[j];
+            int left=height[i];
+            int right=height[j];
             area=Math.max(area,Math.min(left,right)*(j-i));
             if(left<right) i++;
             else j--;
