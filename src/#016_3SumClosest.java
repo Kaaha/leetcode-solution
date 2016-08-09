0100208ca17ecd620d18d133325a0f8fd51c199a
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-09 17:04:08 +0800

    add an O(n^2) solution

--- src/#016_3SumClosest.java
+++ src/#016_3SumClosest.java
@@ -15,3 +15,26 @@ public class Solution {
         return ans;
     }
 }
+
+//solution #2:two pointers
+public class Solution2 {
+    public int threeSumClosest(int[] nums,int target) {
+        int n=nums.length;
+        if(n<3) throw new IllegalArgumentException("No solution.");
+        int ans=nums[0]+nums[1]+nums[2];
+        int i=0;
+        Arrays.sort(nums);
+        while(i<n-2){
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                ans=Math.abs(ans-target)>Math.abs(sum-target)?sum:ans;
+                if(ans==target) return sum;
+                if(sum<target) j++;
+                else k--;
+            }
+            while(nums[i]==nums[++i] && i<n-2);
+        }
+        return ans;
+    }
+}
