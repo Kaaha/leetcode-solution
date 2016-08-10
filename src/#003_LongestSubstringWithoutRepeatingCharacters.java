db588522fae92ddb951e18acee37943880f30806
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-10 15:22:55 +0800

    Update #003_LongestSubstringWithoutRepeatingCharacters.java

--- src/#003_LongestSubstringWithoutRepeatingCharacters.java
+++ src/#003_LongestSubstringWithoutRepeatingCharacters.java
@@ -11,7 +11,7 @@ public class Solution1{
                 i=Math.max(i,map.get(c));
             }
             ans=Math.max(ans,j-i+1);
-            map.put(c,j++);
+            map.put(c,++j);
         }
         return ans;
     }
@@ -30,10 +30,36 @@ public class Solution2 {
                 i++;
             }else{
                 set.add(c);
-                ans=Math.max(ans,j-i);
                 j++;
+                ans=Math.max(ans,j-i);
             }
         }
         return ans;
+    }
+}
+
+//solution #3
+public class Solution3 {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        if(n==0) return 0;
+        int ans=1;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(isUnique(s,i,j)){
+                    ans=Math.max(ans,j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+    private boolean isUnique(String s,int left,int right){
+        Set<Character> set=new HashSet<>();
+        for(int i=left;left<=right;left++){
+            if(set.contains(s.charAt(left)))
+                return false;
+            set.add(s.charAt(left));
+        }
+        return true;
     }
 }
