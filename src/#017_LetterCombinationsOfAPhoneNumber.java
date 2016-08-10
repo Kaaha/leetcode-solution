1291255524416dee68dadd087564f2f6a98e9bf9
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-10 14:48:48 +0800

    Update #017_LetterCombinationsOfAPhoneNumber.java

--- src/#017_LetterCombinationsOfAPhoneNumber.java
+++ src/#017_LetterCombinationsOfAPhoneNumber.java
@@ -19,3 +19,25 @@ public class Solution1 {
         }
     }
 }
+
+//solution #2:FIFO list
+public class Solution2 {
+    private static final String[] KEYS={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits){
+        LinkedList<String> ans=new LinkedList<>();
+        int n=digits.length();
+        if(n==0) return ans;
+        ans.add("");
+        for(int i=0;i<n;i++){
+            while(ans.size()>0 && ans.peek().length()==i){
+                String prefix=ans.remove();
+                String letters=KEYS[digits.charAt(i)-'0'];
+                for(int j=0;j<letters.length();j++){
+                    char c=letters.charAt(j);
+                    ans.add(prefix+c);
+                }
+            }
+        }
+        return ans;
+    }
+}
