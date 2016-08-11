0d147cdb93e6730df6c6702dd9156d1b00710ab7
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-11 19:10:05 +0800

    Update #020_ValidParentheses.java

--- src/#020_ValidParentheses.java
+++ src/#020_ValidParentheses.java
@@ -1,1 +1,35 @@
-
+public class Solution {
+    public boolean isValid(String s){
+        Stack<Character> st=new Stack<>();
+        for(char c:s.toCharArray()){
+            switch(c){
+                case '(':
+                case '{':
+                case '[':st.push(c);break;
+                case ')':{
+                    if(st.empty()) return false;
+                    char t=st.peek();
+                    if(t!='(') return false;
+                    st.pop();
+                    break;
+                }
+                case '}':{
+                    if(st.empty()) return false;
+                    char t=st.peek();
+                    if(t!='{') return false;
+                    st.pop();
+                    break;
+                }
+                case ']':{
+                    if(st.empty()) return false;
+                    char t=st.pop();
+                    if(t!='[') return false;
+                    st.pop();
+                    break;
+                }
+                default:break;
+            }
+            return st.empty();
+        }
+    }
+}
