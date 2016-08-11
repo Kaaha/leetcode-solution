7cc39f8a546014c3eba3cd7893671ecf39975737
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-11 13:08:50 +0800

    Update #019_RemoveNthNodeFromEndOfList.java

--- src/#019_RemoveNthNodeFromEndOfList.java
+++ src/#019_RemoveNthNodeFromEndOfList.java
@@ -27,3 +27,19 @@ public class Solution1 {
         return dummy.next;
     }
 }
+
+//solution #2:one pass by two pointers
+public class Solution2{
+    public ListNode removeNthFromEnd(ListNode head,int n){
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode first=dummy,second=dummy;
+        for(int i=0;i<=n && first!=null;i++) first=first.next;
+        while(first!=null){
+            first=first.next;
+            second=second.next;
+        }
+        second.next=second.next.next;
+        return dummy.next;
+    }
+}
