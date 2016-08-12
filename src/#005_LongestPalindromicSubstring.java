17a364978a9978793f5a9dd281a5357ed73a2f4a
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-12 20:05:25 +0800

    Update #005_LongestPalindromicSubstring.java

--- src/#005_LongestPalindromicSubstring.java
+++ src/#005_LongestPalindromicSubstring.java
@@ -1,1 +1,29 @@
-
+//solution #2
+public class Solution1 {
+    public String longestPalindrome(String s) {
+        int n=s.length();
+        boolean[][] table=new boolean[1000][1000];
+        for(int i=0;i<n;i++) table[i][i]=true;
+        int maxLen=1;
+        int begin=0;
+        for(int i=0;i<n-1;i++){
+            if(s.charAt(i)==s.charAt(i+1)){
+                table[i][i+1]=true;
+                maxLen=2;
+                begin=i;
+            }
+        }
+        
+        for(int len=3;len<=n;len++){
+            for(int i=0;i<=n-len+1;i++){
+                int j=len+i-1;
+                if(table[i+1][j-1] && s.charAt(i)==s.charAt(j)){
+                    table[i][j]=true;
+                    begin=i;
+                    maxLen=len;
+                }
+            }
+        }
+        return s.substring(begin,begin+maxLen);
+    }
+}
