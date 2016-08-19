63180200af7b2f02dbdc52fcdf0b9339a6afa071
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-19 19:16:59 +0800

    Update #021_MergeTwoSortedLists.java

--- src/#021_MergeTwoSortedLists.java
+++ src/#021_MergeTwoSortedLists.java
@@ -33,3 +33,18 @@ public class Solution1 {
         return dummy.next;
     }
 }
+
+//solution #2
+public class Solution2 {
+    public ListNode mergeTwoLists(listNode l1,ListNode l2) {
+        if(l1==null) return l1;
+        if(l2==null) return l2;
+        if(l1.val<l2.val) {
+            l1=mergeTwoLists(l1.next,l2);
+            return l1;
+        }else {
+            l2=mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+}
