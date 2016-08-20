707854baf8248c648b28ea07ae473bf16eb0e3fc
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-20 20:29:14 +0800

    Update #022_GenerateParentheses.java

--- src/#022_GenerateParentheses.java
+++ src/#022_GenerateParentheses.java
@@ -1,1 +1,20 @@
-
+public class Solution {
+    public List<String> genereateParenthesis(int n) {
+        List<String> ans=new ArrayList<>();
+        if(n<=0) return ans;
+        generateParenthesis("",n,n,ans);
+        return ans;
+    }
+    private void generateParenthesis(String s,int l,int r,List<String> ans) {
+        if(r==0) {
+            ans.add(s);
+            return;
+        }
+        if(l>0) {
+            generateParenthesis(s+'(',l-1,r,ans);
+        }
+        if(l<r) {
+            generateParenthesis(s+')',l,r-1,ans);
+        }
+    }
+}
