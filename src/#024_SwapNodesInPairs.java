ef6c162fed530c29939a2a00df2a879e026e6d5b
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-21 20:22:11 +0800

    Update #024_SwapNodesInPairs.java

--- src/#024_SwapNodesInPairs.java
+++ src/#024_SwapNodesInPairs.java
@@ -1,1 +1,20 @@
-
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy=new ListNode(0);
+        dymmy.next=head;
+        ListNode p=head,q;
+        ListNode prev=dummy;
+        while(p!=null && p.next!=null) {
+            q=p.next;
+            //swap p and q
+            prev.next=q;
+            p.next=q.next;
+            q.next=p;
+            //change prev
+            prev=p;
+            //change p
+            p=p.next;
+        }
+        return dummy.next;
+    }
+}
