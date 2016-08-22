e8a3f4aeb2e0ffae158f622c4faa488ca2cfaf98
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-22 14:09:21 +0800

    Update #206_ReverseLinkedList.java

--- src/#206_ReverseLinkedList.java
+++ src/#206_ReverseLinkedList.java
@@ -1,1 +1,16 @@
-
+//solution #1
+public class Solution1 {
+    public ListNode reverseList(ListNode head) {
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode p=dummy.next,q;
+        while(p!=null && p.next!=null) {
+            q=p.next;
+            ListNode temp=dummy.next;
+            dummy.next=q;
+            p.next=q.next;
+            q.next=temp;
+        }
+        return dummy.next;
+    }
+}
