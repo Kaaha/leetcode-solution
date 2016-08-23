8ea5366026dff3c43370df4799e83c72045a2913
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-23 09:59:08 +0800

    Update #027_RemoveElement.java

--- src/#027_RemoveElement.java
+++ src/#027_RemoveElement.java
@@ -1,1 +1,11 @@
-
+//solution #1
+public class Solution1 {
+    public int removeElement(int[] nums,int val) {
+        int n=0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]!=val)
+                nums[n++]=nums[i];
+        }
+        return n;
+    }
+}
