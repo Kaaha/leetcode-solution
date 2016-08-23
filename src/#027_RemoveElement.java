fe8f38b3057ea57408d8acca43e1e68886f6ec95
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-23 10:10:21 +0800

    Update #027_RemoveElement.java

--- src/#027_RemoveElement.java
+++ src/#027_RemoveElement.java
@@ -9,3 +9,21 @@ public class Solution1 {
         return n;
     }
 }
+
+//solution #2
+public class Solution2 {
+    public int removeElement(int[] nums,int val) {
+        int i=0,j=nums.length-1;
+        while(i<=j) {
+            while(i<=j && nums[i]!=val) i++;
+            while(i<=j && nums[j]==val) j--;
+            if(i<=j) {
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;j--;
+            }
+        }
+        return i;
+    }
+}
