45a74ef9ec0d2c5d687c84e781b0f2be625c03df
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-23 09:26:52 +0800

    Update #025_ReverseNodesInK-Group.java

--- src/#025_ReverseNodesInK-Group.java
+++ src/#025_ReverseNodesInK-Group.java
@@ -1,1 +1,28 @@
-
+public class Solution {
+    public ListNode reverseKGroup(ListNode head,int k) {
+        if(k==1) return head;
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode p,q;
+        ListNode prev=dummy,end=head;
+        int steps=0;
+        while(end!=null) {
+            while(steps%k!=k-1 && end!=null) {
+                end=end.next;
+                steps++;
+            }
+            if(end==null) return dummy.next;
+            steps++;end=end.next;
+            p=prev.next;
+            while(p!=end && p.next!=end) {
+                q=p.next;
+                ListNode temp=prev.next;
+                prev.next=q;
+                p.next=q.next;
+                q.next=temp;
+            }
+            prev=p;
+        }
+        return dummy.next;
+    }
+}
