c00604df45b9b8779223e6643b99f25ace17e7a0
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-23 10:40:09 +0800

    Update #028_ImplementstrStr().java

--- src/#028_ImplementstrStr().java
+++ src/#028_ImplementstrStr().java
@@ -1,1 +1,13 @@
-
+public class Solution {
+    int strStr(String haystack,String needle) {
+        int m=haystack.length();
+        int n=needle.length();
+        if(m<n) return -1;
+        else if(n==0) return 0;
+        for(int i=0;i<=m-n;i++) {
+            if(haystack.substring(i,i+n).equals(needle))
+                return i;
+        }
+        return -1;
+    }
+}
