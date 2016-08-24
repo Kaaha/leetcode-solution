566527c626ca458180f012764069da425423953d
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-24 22:25:49 +0800

    Update #030_SubstringWithConcatenationOfAllWords.java

--- src/#030_SubstringWithConcatenationOfAllWords.java
+++ src/#030_SubstringWithConcatenationOfAllWords.java
@@ -1,1 +1,23 @@
-
+//solution #1:time limit exceed
+public class Solution1 {
+    public List<Integer> findSubstring(String s,String[] words) {
+        List<Integer> ans=new ArrayList<>();
+        int wordNum=words.length;
+        int len=words[0].length();
+        Map<String,Integer> map=new HashMap<>();
+        for(String w:words) {
+            map.put(w,map.containsKey(w)?map.get(w)+1:1);
+        }
+        for(int i=0;i+len*wordNum<s.length();i++) {
+            Map<String,Integer> visit=new HashMap<>();
+            for(int j=0;j<wordNum;j++) {
+                String word=s.substring(j,j+len);
+                if(!map.containsKey(word)) break;
+                visit.put(word,visit.containsKey(word)?visit.get(word)+1:1);
+            }
+            if(visit.equals(map)) 
+                ans.add(i);
+        }
+        return ans;
+    }
+}
