b9c4ab005c1870b541bb9ff49519b03173593263
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-24 17:44:27 +0800

    Update #029_DivideTwoIntegers.java

--- src/#029_DivideTwoIntegers.java
+++ src/#029_DivideTwoIntegers.java
@@ -1,1 +1,20 @@
-
+public class Solution {
+    public int divide(int dividend,int divisor) {
+        if(dividend==Integer.MIN_VALUE && divisor==-1 || divisor==0)
+            return Integer.MAX_VALUE;
+        long divd=Math.abs((long)dividend);
+        long divs=Math.abs((long)divisor);
+        int ans=0;
+        while(divd>=divs) {
+            long temp=divs;
+            int mult=1;
+            while(divd>=(temp<<1)) {
+                temp=temp<<1;
+                mult=mult<<1;
+            }
+            divd-=temp;
+            ans+=mult;
+        }
+        return ((dividend>0) ^ (divisor>0))?-ans:ans;
+    }
+}
