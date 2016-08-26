b711011ab4f18e7cbfeb1dcd869803c94bcb0c04
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-26 19:28:05 +0800

    Update #032_LongestValidParentheses.java

--- src/#032_LongestValidParentheses.java
+++ src/#032_LongestValidParentheses.java
@@ -1,1 +1,24 @@
-
+public class Solution {
+    public int longestValidParentheses(String s) {
+        int ans=0;
+        Stack<Integer> st=new Stack<>();
+        int start=-1;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)=='(') {
+                st.push(i);
+            }else {
+                if(st.isEmpty()) {
+                    start=i;
+                }else {
+                    st.pop();
+                    if(st.isEmpty()) {
+                        ans=Math.max(ans,i-start);
+                    }else {
+                        ans=Math.max(ans,i-st.peek());
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
