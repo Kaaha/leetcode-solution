1a48de3874f1b6baa2c81089243f6be8c57abb08
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-27 22:00:16 +0800

    Update #035_SearchInsertPosition.java

--- src/#035_SearchInsertPosition.java
+++ src/#035_SearchInsertPosition.java
@@ -12,3 +12,18 @@ public class Solution1 {
         return lo;
     }
 }
+
+//solution #2
+public class Solution2 {
+    public int searchInsert(int[] nums,int target) {
+        int n=nums.length;
+        int lo=0,hi=n-1;
+        while(lo<=hi) {
+            int mid=(lo+hi)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) lo=mid+1;
+            else hi=mid-1;
+        }
+        return lo;
+    }
+}
