4ec4c0dce5b6fe1006be3518d62cccbc8f15a4bb
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-27 21:30:45 +0800

    Update #034_SearchForAReage.java

--- src/#034_SearchForAReage.java
+++ src/#034_SearchForAReage.java
@@ -1,4 +1,5 @@
-public class Solution {
+//solution #1
+public class Solution1 {
     public int[] searchRange(int[] nums,int target) {
         int[] ans=new int[]{-1,-1};
         int n=nums.length;
@@ -20,4 +21,28 @@ public class Solution {
         ans[j]=j-1;
         return ans;
     }
+}
+
+//solution #2
+public class Solution2 {
+    int[] ans=new int[]{-1,-1};
+    int n=nums.length;
+    int lo=0,hi=n-1;
+    //find the left boundary
+    while(lo<hi) {
+        int mid=(lo+hi)/2;
+        if(nums[mid]<target) lo=mid+1;//the left boundary is on the right side
+        else hi=mid;//the left boundary is on the left side
+    }
+    if(nums[lo]!=target) return ans;
+    ans[0]=lo;
+    hi=n-1;
+    //find the right boundary
+    while(lo<hi) {
+        int mid=(lo+hi)/2+1;//make the mid biased to hi
+        if(nums[mid]>target) hi=mid-1;//the right boundary is on the left side
+        else lo=mid;//the right boundary is on the right side
+    }
+    ans[1]=hi;
+    return ans;
 }
