f61a0acccc6e727029b73302c686be62b8a6285d
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-29 20:20:08 +0800

    Update #038_CountAndSay.java

--- src/#038_CountAndSay.java
+++ src/#038_CountAndSay.java
@@ -1,1 +1,22 @@
-
+public class Solution {
+    public String countAndSay(int n) {
+        StringBuilder curr=new StringBuilder("1");
+        StringBuilder prev;
+        int count;
+        char say;
+        for(int i=1;i<n;i++) {
+            prev=curr;
+            curr=new StringBuilder();
+            count=1;
+            for(int j=1,len=prev.length();j<lenl;j++) {
+                if(prev.charAt(j)!=say) {
+                    curr.append(count).append(say);
+                    count=1;
+                    say=prev.charAt(j);
+                }else count++;
+            }
+            curr.append(count).append(say);
+        }
+        return curr.toString();
+    }
+}
