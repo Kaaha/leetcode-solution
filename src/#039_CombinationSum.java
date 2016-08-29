d7fa75ace46466891e03fa2540641d32370032fb
Liu Guoqiang <liuguoqiang1573@163.com>
2016-08-29 21:26:13 +0800

    Update #039_CombinationSum.java

--- src/#039_CombinationSum.java
+++ src/#039_CombinationSum.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates,int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        combination(ans,list,candidates,target,0);
+        return ans;
+    }
+    private void combination(List<List<Integer>> ans,List<Integer> list,int target,int begin) {
+        if(target==0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=begin;i<candidates.length && target>=candidates[i];i++) {
+            list.add(candidates[i]);
+            combination(ans,list,target-candidates[i],i);
+            list.remove(list.size()-1);
+        }
+    }
+}
