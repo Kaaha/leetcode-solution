2618f22484e73c50e21e45109ced232512623e37
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-05 21:43:01 +0800

    Update #007_ReserveInteger.java

--- src/#007_ReserveInteger.java
+++ src/#007_ReserveInteger.java
@@ -1,1 +1,16 @@
-
+public class Solution {
+    public int reverse(int x) {
+        int ans=0;
+        int sign=x>0?1:-1;
+        x*=sign;
+        while(x>0){
+            int least=x%10;
+            int t=ans;
+            ans=ans*10+least;
+            int test=(ans-least)/10;
+            if(test!=t)return 0;
+            x/=10;
+        }
+        return ans*sign;
+    }
+}
