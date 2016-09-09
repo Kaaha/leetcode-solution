d90ecb03c5c05da03cfee6fa4d802363c1314c91
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-09 20:43:41 +0800

    Update #048_RotateImage.java

--- src/#048_RotateImage.java
+++ src/#048_RotateImage.java
@@ -1,1 +1,18 @@
-
+//solution #1
+public class Solution1 {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int[][] temp=new int[n][n];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                temp[i][j]=matrix[n-j-1][i];
+            }
+        }
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                matrix[i][j]=temp[i][j];
+            }
+        }
+    }
+}
