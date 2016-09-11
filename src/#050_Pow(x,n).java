307cdc34eb98b8e659c42eaba0c909760d5949f5
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-11 21:07:00 +0800

    Update #050_Pow(x,n).java

--- src/#050_Pow(x,n).java
+++ src/#050_Pow(x,n).java
@@ -1,1 +1,12 @@
-
+public class Solution {
+    public double myPow(double x,int n) {
+        double ans=1;
+        long N=Math.abs((long)n);
+        while(N>0) {
+            if((N&1)==1) ans*=x;
+            N=N>>1;
+            x*=x;
+        }
+        return n>0?ans:1/ans;
+    }
+}
