40de8525a66e4242935ee9f34765a6e428d4db1f
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-11 20:11:43 +0800

    Update #049_GroupAnagrams.java

--- src/#049_GroupAnagrams.java
+++ src/#049_GroupAnagrams.java
@@ -1,1 +1,15 @@
-
+public class  Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null ||strs.length==0) return new ArrayList<List<String>>();
+        Map<String,List<String>> map=new HashMap<>();
+        Arrays.sort(strs);
+        for(String s:strs) {
+            char[] cs=s.toCharArray();
+            Arrays.sort(cs);
+            String key=String.valueOf(cs);
+            if(!map.containsKey(key)) map.put(key,new ArrayList<List<String>>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<List<String>>(map.values);
+    }
+}
