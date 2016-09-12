391e2b13a786da0c5983c531178fc13257a236c4
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-12 22:38:04 +0800

    Update #053_MaximumSubarray.java

--- src/#053_MaximumSubarray.java
+++ src/#053_MaximumSubarray.java
@@ -1,5 +1,6 @@
-public class Solution {
-    public int maxSubarray(int[] nums) {
+//solution #1:dynamic programming
+public class Solution1 {
+    public int maxSubArray(int[] nums) {
         int n=nums.length;
         int[] dp=new int[n];
         dp[0]=nums[0];
@@ -9,5 +10,34 @@ public class Solution {
             max=Math.max(max,dp[i]);
         }
         return max;
+    }
+}
+
+//solution #2:divide and conquer
+public class Solution2 {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        
+    }
+    private int maxSubArray(int[] nums,int lo,int hi) {
+        if(lo==hi) return nums[lo];
+        int mid=lo+(hi-lo)/2;
+        int maxLeft=maxSubArray(nums,lo,mid);
+        int maxRight=maxSubArray(nums,mid+1,hi);
+        int maxMid,sum=nums[mid],t=sum;
+        for(int i=mid-1;i>=lo;i--) {
+            sum+=nums[i];
+            t=Math.max(sum,t);
+        }
+        maxMid=t;
+        sum=nums[mid+1];
+        t=nums[mid+1];
+        for(int i=mid+2;i<=hi;i++) {
+            sum+=nums[i];
+            t=Math.max(sum,t);
+        }
+        if(maxMid+t<0) maxMid=Math.max(maxMid,t);
+        else maxMid+=t;
+        return Math.max(maxMid,Math.max(maxLeft,maxRight));
     }
 }
