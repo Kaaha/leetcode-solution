96bb6494bb8ebe61a32250dacfce5519021c2cbd
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-16 20:49:06 +0800

    Update #045_JumpGameII.java

--- src/#045_JumpGameII.java
+++ src/#045_JumpGameII.java
@@ -1,1 +1,16 @@
-
+public class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int step=0;
+        int max=0;
+        int e=0;
+        for(int i=0;i<n-1;i++) {
+            max=Math.max(max,i+nums[i]);
+            if(i==e) {
+                step++;
+                e=max;
+            }
+        }
+        return step;
+    }
+}
