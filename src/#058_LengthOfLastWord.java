11b5a7a78d90006d12078ce8e3b84b919eaf2a0a
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-19 12:45:29 +0800

    Update #058_LengthOfLastWord.java

--- src/#058_LengthOfLastWord.java
+++ src/#058_LengthOfLastWord.java
@@ -1,1 +1,7 @@
-
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        String ss=s.trim();
+        String[] strs=ss.split(" ");
+        return strs[strs.length-1].length();
+    }
+}
