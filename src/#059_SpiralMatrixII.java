cdf0d4c5ac525a55621c57c946dd9461432e7ade
Liu Guoqiang <liuguoqiang1573@163.com>
2016-09-19 13:11:28 +0800

    Update #059_SpiralMatrixII.java

--- src/#059_SpiralMatrixII.java
+++ src/#059_SpiralMatrixII.java
@@ -1,1 +1,31 @@
-
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] ans=new int[n][n];
+        int rowBegin=0,rowEnd=n-1;
+        int colBegin=0,colEnd=n-1;
+        int num=1;
+        while(rowBegin<=rowEnd && colBegin<=colEnd) {
+            //right
+            for(int i=colBegin;i<=colEnd;i++) {
+                ans[colBegin][i]=num++;
+            }
+            rowBegin++;
+            //down
+            for(int i=rowBegin;i<=rowEnd;i++) {
+                ans[i][rowEnd]=num++;
+            }
+            colEnd--;
+            //left
+            for(int i=colEnd;i>=colBegin;i--) {
+                if(rowBegin>rowEnd) break;
+                ans[rowEnd][i]=num++;
+            }
+            //up
+            for(int i=rowEnd;i>=rowBegin;i--) {
+                if(colBegin>colEnd) break;
+                ans[i][colBegin]=num++;
+            }
+        }
+        return ans;
+    }
+}
