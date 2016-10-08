951ece3bb3f9595d1e405d55cd31cfab96d82286
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-08 22:17:57 +0800

    Update #060_PermutationSequence.java

--- src/#060_PermutationSequence.java
+++ src/#060_PermutationSequence.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+    String getPermutation(int n,int k){
+        StringBuilder sb=new StringBuilder();
+        List<Integer> digits=new ArrayList<>();
+        int[] fact=new int[n+1];
+        fact[0]=1;
+        for(int i=1;i<=n;i++) {
+            fact[i]=fact[i-1]*i;
+            digits.add(i);
+        }
+        k--;
+        for(int i=n-1;i>=0;i--) {
+            int index=k/fact[i];
+            sb.append(digits.get(index));
+            digits.remove(index);
+        }
+        return sb.toString();
+    }
+}
