20ef00c07eb8ad45186fb118b73a77785713d198
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-09 22:02:11 +0800

    Update #064_MinimumPathSum.java

--- src/#064_MinimumPathSum.java
+++ src/#064_MinimumPathSum.java
@@ -1,1 +1,15 @@
-
+public class Solution {
+    public int public int minPathSum(int[][] grid) {
+        int m=grid[0].length;
+        int[] table=new int[m];
+        table[0]=grid[0][0];
+        for(int i=1;i<m;i++) table[i]=table[i-1]+grid[0][i];
+        for(int i=1;i<grid.length;i++) {
+            for(int j=0;j<m;j++) {
+                if(j>0) table[j]=Math.min(table[j],table[j-1])+grid[i][j];
+                else table[j]+=grid[i][j];
+            }
+        }
+        return table[m-1];
+    }
+}
