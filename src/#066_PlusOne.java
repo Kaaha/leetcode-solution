9fdc53b7df71190cafdbf6600311e15e9984b00a
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-10 21:59:16 +0800

    Update #066_PlusOne.java

--- src/#066_PlusOne.java
+++ src/#066_PlusOne.java
@@ -1,1 +1,17 @@
-
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        int carry=1;
+        for(int i=n-1;i>=0;i--) {
+            int sum=carry+digits[i];
+            carry=sum/10;
+            digits[i]=sum%10;
+        }
+        if(carry==0) return digits;
+        else {
+            int[] ans=new int[n+1];
+            ans[0]=1;
+            return ans;
+        }
+    }
+}
