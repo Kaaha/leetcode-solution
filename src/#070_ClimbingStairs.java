367890e45a54388211b899de41581b38d3d4fb35
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-12 22:53:04 +0800

    Update #070_ClimbingStairs.java

--- src/#070_ClimbingStairs.java
+++ src/#070_ClimbingStairs.java
@@ -1,1 +1,14 @@
-
+public class Solution {
+    public int climbStairs(int n) {
+        if(n<=0) return 1;
+        int a=0,b=1;
+        int i=0,ans=0;
+        while(i<n) {
+            ans=a+b;
+            a=b;
+            b=ans;
+            i++;
+        }
+        return ans;
+    }
+}
