ba84ab840f786ac0e55f2a128fba32c58b938a1e
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-12 22:44:44 +0800

    Update #069_Sqrt(x).java

--- src/#069_Sqrt(x).java
+++ src/#069_Sqrt(x).java
@@ -1,1 +1,13 @@
-
+public class Solution {
+    public int mySqrt(int x) {
+        if(x<0) return x;
+        int lo=0,hi=43461;
+        while(lo<hi-1) {
+            int mid=(lo+hi)/2;
+            if(mid*mid<x) lo=mid;
+            else if(mid*mid>x) hi=mid;
+            else return mid;
+        }
+        return lo;
+    }
+}
