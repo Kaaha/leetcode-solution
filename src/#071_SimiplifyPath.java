39a9a8e75b5b02d7e484faf32994abf17b531834
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-13 21:10:08 +0800

    Update #071_SimiplifyPath.java

--- src/#071_SimiplifyPath.java
+++ src/#071_SimiplifyPath.java
@@ -1,1 +1,16 @@
-
+public class Solution {
+    public String simiplifyPath(String path) {
+        String[] ss=path.split("\");
+        List<String> st=new ArrayList<>();
+        for(String s:ss) {
+            if(s.equals("")) continue;
+            else if(s.equals(".")) continue;
+            else if(s.equals("\")) {
+                if(st.size()>0) st.remove(st.size()-1);                 
+            }
+            else st.add(s);
+        }
+        String ans="/"+String.join("/",st.toArray(new String[0]));
+        return ans;
+    }
+}
