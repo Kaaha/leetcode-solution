0261f3f2135cb1320936c0c6868120ba50cf140d
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-14 21:29:08 +0800

    Update #073_SetMatrixZeroes.java

--- src/#073_SetMatrixZeroes.java
+++ src/#073_SetMatrixZeroes.java
@@ -20,3 +20,38 @@ public class Solution1 {
         }
     }
 }
+
+//solution #2
+public class Solution2 {
+    public void setMatrixZeroes(int[][] matrix) {
+        int m=matrix.length,n=matrix[0].length;
+        boolean left=false,up=false;
+        for(int i=0;i<m;i++) if(matrix[i][0]==0) {
+            left=true;
+            break;
+        }
+        for(int j=0;j<n;j++) if(matrix[0][j]==0) {
+            up=true;
+            break;
+        }
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(matrix[i][j]==0) {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<m;i++) {
+            if(matrix[i][0]==0) for(int j=1;j<n;j++) matrix[i][j]=0;
+        }
+        for(int j=1;j<n;j++) {
+            if(matrix[0][j]==0) for(int i=1;i<m;i++) matrix[i][j]=0;
+        }
+        
+        if(left) for(int i=0;i<m;i++) matrix[i][0]=0;
+        if(up) for(int j=0;j<n;j++) matrix[0][j]=0;
+    }
+}
