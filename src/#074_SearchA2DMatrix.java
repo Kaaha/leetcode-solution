b0ed542456941eb3be1e16340e13411f37cbac71
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-14 21:59:28 +0800

    Update #074_SearchA2DMatrix.java

--- src/#074_SearchA2DMatrix.java
+++ src/#074_SearchA2DMatrix.java
@@ -1,1 +1,16 @@
-
+public class Solution {
+    public boolean searchMatrix(int[][] matrix,int target) {
+        int m=matrix.length;
+        if(m==0) return false;
+        int n=matrix[0].length;
+        int lo=0,hi=m*n-1;
+        while(lo<=hi) {
+            int mid=(lo+hi)/2;
+            int i=mid/n,j=mid%n;
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]<target) lo=mid+1;
+            else hi=mid-1;
+        }
+        return false;
+    }
+}
