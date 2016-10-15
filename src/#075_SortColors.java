e6ab644488565e25d364ea4011cf54f66a3d4ca0
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-15 19:28:42 +0800

    Update #075_SortColors.java

--- src/#075_SortColors.java
+++ src/#075_SortColors.java
@@ -13,3 +13,19 @@ public class Solution1 {
         while(i<red+white+blue) nums[i++]=2;
     }
 }
+
+//solution #2
+public class Solution2 {
+    public void sortColors(int[] nums) {
+        int zero=0,two=nums.length-1;
+        for(int i=0;i<=two;i++) {
+            while(nums[i]==2 && i<two) swap(nums,i,two--);
+            while(nums[i]==0 && i>zero) swap(nums,i,zero++);
+        }
+    }
+    private void swap(int[] nums,int i,int j) {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
