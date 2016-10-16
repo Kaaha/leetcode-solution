b39794f6d0db901a705ebc3e54fff5d51848399d
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-16 23:07:04 +0800

    Update #072_EditDistance.java

--- src/#072_EditDistance.java
+++ src/#072_EditDistance.java
@@ -1,4 +1,5 @@
-public class Solution {
+//solution #1
+public class Solution1 {
     public int minDistance(String word1,String word2) {
         int m=word1.length(),n=word2.length();
         int[][] table=new int[m][n];
@@ -15,5 +16,32 @@ public class Solution {
             }
         }
         return table[m][n];
+    }
+}
+
+//solution #2
+public class Solution2 {
+    public int minDistance(String word1,String word2) {
+        int m=word1.length(),n=word2.length();
+        if(m==0) return n;
+        if(n==0) return m;
+        int[][] table=new int[m][n];
+        
+    }
+    private int getMinDistance(String word1,String word2,int i,int j,int[][] table) {
+        if(i<0 && j<0) return 0;
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        if(table[i][j]!=0) return table[i][j];
+        if(word1.charAt(i)==word2.charAt(j)) {
+            table[i][j]=getMinDistance(word1,word2,i-1,j-1,table);
+            return table[i][j];
+        }
+        int insert=getMinDistance(word1,word2,i-1,j,table)+1;
+        int delete=getMinDistance(word1,word2,i,j-1,table)+1;
+        int alter=getMinDistance(word1,word2,i-1,j-1,table)+1;
+        int tmp=Math.min(insert,delete);
+        table[i][j]=Math.min(tmp,alter);
+        return table[i][j];
     }
 }
