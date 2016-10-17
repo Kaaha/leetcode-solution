187e2a37bf8db4cfd8a12debdbac29ec6dd32301
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-17 21:57:56 +0800

    Update #077_Combinations.java

--- src/#077_Combinations.java
+++ src/#077_Combinations.java
@@ -19,3 +19,26 @@ public class Solution1 {
         }
     }
 }
+
+//solution #2
+public class Solution2 {
+    public List<List<Integer>> combine(int n,int k) {
+        List<List<Integer>> ans=new LinkedList<>();
+        if(n<k || n==0) return ans;
+        int[] nums=new int[k];
+        genList(ans,nums,n,1,0);
+        return ans;
+    }
+    private void genList(List<List<Integer>> ans,int[] nums,int n,int idx,int count) {
+        if(count==nums.length) {
+            List<Integer> tmp=new LinkedList<>();
+            for(int a:nums) tmp.add(a);
+            ans.add(tmp);
+            return;
+        }
+        for(int i=idx;i<=n;i++) {
+            nums[count]=i;
+            genList(ans,nums,n,idx+1,count+1);
+        }
+    }
+}
