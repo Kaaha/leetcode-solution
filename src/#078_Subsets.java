f444a28f92ec04071153cddbded3007f13f85d84
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-19 22:44:32 +0800

    Update #078_Subsets.java

--- src/#078_Subsets.java
+++ src/#078_Subsets.java
@@ -1,1 +1,13 @@
-
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new LinkedList<>();
+        int n=nums.length,total=(int)Math.pow(2,n);
+        for(int i=0;i<total;i++) ans.add(new ArrayList<Integer>());
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<total;j++) {
+                if(((j>>i)&1)==1) lists.get(j).add(nums[i]);
+            }
+        }
+        return ans;
+    }
+}
