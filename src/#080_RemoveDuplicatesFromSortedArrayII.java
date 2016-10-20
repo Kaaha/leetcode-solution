9e6121ba1d1c661ebc57d888fc235cbb5e865308
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-20 23:12:20 +0800

    Update #080_RemoveDuplicatesFromSortedArrayII.java

--- src/#080_RemoveDuplicatesFromSortedArrayII.java
+++ src/#080_RemoveDuplicatesFromSortedArrayII.java
@@ -1,1 +1,10 @@
-
+public class Solution {
+    public int removeDupllicates(int[] nums) {
+        int i=0;
+        for(int n:nums) {
+            if(i<2 || n>nums[i-2])
+                nums[i++]=n;
+        }
+        return i;
+    }
+}
