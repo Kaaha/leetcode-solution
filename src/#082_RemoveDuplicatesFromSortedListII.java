34b93df3728864a1243e5830a098b6348c99b52c
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-23 22:45:20 +0800

    Update #082_RemoveDuplicatesFromSortedListII.java

--- src/#082_RemoveDuplicatesFromSortedListII.java
+++ src/#082_RemoveDuplicatesFromSortedListII.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode p=head;
+        while(p!=null && p.next!=null) {
+            if(p.val==p.next.val) {
+                ListNode q=p.next;
+                while(q!=null && q.val==p.val) q=q.next;
+                p=q;
+                pre.next=p;
+            }else {
+                pre=p;
+                p=p.next;
+            }
+        }
+        return dummy.next;
+    }
+}
