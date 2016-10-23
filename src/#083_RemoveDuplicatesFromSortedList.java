6fe1ad5ac1f974beb76b7f882f83be786b0c9dc7
Liu Guoqiang <liuguoqiang1573@163.com>
2016-10-23 22:17:09 +0800

    Update #083_RemoveDuplicatesFromSortedList.java

--- src/#083_RemoveDuplicatesFromSortedList.java
+++ src/#083_RemoveDuplicatesFromSortedList.java
@@ -1,1 +1,16 @@
-
+public class Solution {
+    public ListNode removeDuplicates(ListNode head) {
+        if(head==null) return head;
+        ListNode p=head,q=p.next;
+        while(q!=null) {
+            if(p.val==q.val) {
+                p.next=q.next;
+                q=p.next;
+            }else {
+                p=q;
+                q=p.next;
+            }
+        }
+        return head;
+    }
+}
